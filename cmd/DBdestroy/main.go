6d6a7c04632188877fd07dbedd97a2065cff1f7e
cyla00 <ikhayam000@protonmail.com>
2023-12-25 19:32:20 +0100

    db destroy source

--- cmd/DBdestroy/main.go
+++ cmd/DBdestroy/main.go
@@ -1,14 +1,39 @@
 package main
 
-func main() {
-	// err := godotenv.Load()
-	// if err != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
 
-	// connStr := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", os.Getenv("DB_USER"), os.Getenv("DB_PWD"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
-	// db, err := sql.Open("postgres", connStr)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	connStr := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", os.Getenv("DB_USER"), os.Getenv("DB_PWD"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	drop_users_err := db.QueryRow("DROP TABLE users")
+	if drop_users_err != nil {
+		fmt.Println("", drop_users_err.Err())
+	}
+
+	drop_patterns_err := db.QueryRow("DROP TABLE patterns")
+	if drop_patterns_err != nil {
+		fmt.Println("", drop_patterns_err.Err())
+	}
+
+	drop_comments_err := db.QueryRow("DROP TABLE comments")
+	if drop_comments_err != nil {
+		fmt.Println("", drop_comments_err.Err())
+	}
 }
