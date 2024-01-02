93bfdfc43bea40c755d7705da1c3ca73475bcf7a
cyla00 <ikhayam000@protonmail.com>
2024-01-02 03:14:40 +0100

    habndlerFunc for middlewared routes

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"myog/api"
 	"myog/database"
+	"myog/middleware"
 	"myog/templates/index"
 	"myog/templates/login"
 	"myog/templates/signup"
@@ -36,14 +37,22 @@ func main() {
 	http.Handle("/login", templ.Handler(login.LoginPage()))
 
 	// API
+	uploadPatternHandler := http.HandlerFunc(api.PatternUploadRoute)
+	deletePatternHandler := http.HandlerFunc(api.PatternDeleteRoute)
+	modifyPatternHandler := http.HandlerFunc(api.PatternModifyRoute)
+	commentPatternHandler := http.HandlerFunc(api.PatternCommentRoute)
+	modifyUserHandler := http.HandlerFunc(api.UserModifyRoute)
+	deleteUserHandler := http.HandlerFunc(api.UserDeleteRoute)
+
 	http.HandleFunc("/api/"+os.Getenv("API_VERSION")+"/login", api.LoginRoute)
 	http.HandleFunc("/api/"+os.Getenv("API_VERSION")+"/registration", api.RegistrationRoute)
-	http.HandleFunc("/api/"+os.Getenv("API_VERSION")+"/upload-pattern", api.PatternUploadRoute)
-	http.HandleFunc("/api/"+os.Getenv("API_VERSION")+"/delete-pattern", api.PatternDeleteRoute)
-	http.HandleFunc("/api/"+os.Getenv("API_VERSION")+"/modify-pattern", api.PatternModifyRoute)
-	http.HandleFunc("/api/"+os.Getenv("API_VERSION")+"/comment-pattern", api.PatternCommentRoute)
-	http.HandleFunc("/api/"+os.Getenv("API_VERSION")+"/modify-user", api.UserModifyRoute)
-	http.HandleFunc("/api/"+os.Getenv("API_VERSION")+"/delete-user", api.UserDeleteRoute)
+	http.Handle("/api/"+os.Getenv("API_VERSION")+"/upload-pattern", middleware.AuthMiddleware(uploadPatternHandler))
+	http.Handle("/api/"+os.Getenv("API_VERSION")+"/delete-pattern", middleware.AuthMiddleware(deletePatternHandler))
+	http.Handle("/api/"+os.Getenv("API_VERSION")+"/modify-pattern", middleware.AuthMiddleware(modifyPatternHandler))
+
+	http.Handle("/api/"+os.Getenv("API_VERSION")+"/comment-pattern", middleware.AuthMiddleware(commentPatternHandler))
+	http.Handle("/api/"+os.Getenv("API_VERSION")+"/modify-user", middleware.AuthMiddleware(modifyUserHandler))
+	http.Handle("/api/"+os.Getenv("API_VERSION")+"/delete-user", middleware.AuthMiddleware(deleteUserHandler))
 
 	fmt.Printf("running on localhost:%s", os.Getenv("SERVER_PORT"))
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("SERVER_PORT"), nil))
